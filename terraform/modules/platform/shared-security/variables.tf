b729c34e8fb861f9f0aee4b716aa9891d392fb1a
wladysson <wladysonaraujo991@gmail.com>
2026-03-27 14:00:00 -0300

    adiciona variables, padroniza parametrizaçao

---
+++ terraform/modules/platform/shared-security/variables.tf
@@ -0,0 +1,9 @@
+variable "environment" {
+  type        = string
+  description = "Deployment environment"
+}
+
+variable "cluster_name" {
+  type        = string
+  description = "EKS cluster name"
+}
