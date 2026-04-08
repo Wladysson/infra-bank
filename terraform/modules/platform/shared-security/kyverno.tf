7e9fb9fb6cb74534cd006c6e9d82aa59ddb7cd63
wladysson <wladysonaraujo991@gmail.com>
2026-04-08 14:00:00 -0300

    instala kiverno para politicas kubernetes, adiciona validaçaoo e mutaçao de recursos, reforça complice e governança

---
+++ terraform/modules/platform/shared-security/kyverno.tf
@@ -0,0 +1,6 @@
+resource "helm_release" "kyverno" {
+  name       = "kyverno"
+  repository = "https://kyverno.github.io/kyverno"
+  chart      = "kyverno"
+  namespace  = kubernetes_namespace.security.metadata[0].name
+}
