e227c85d7c24a9b4db28ebdd566cfade5e53255e
wladysson <wladysonaraujo991@gmail.com>
2026-04-12 14:00:00 -0300

    adiciona variables do modulo de ci-cd

---
+++ terraform/modules/platform/ci-cd-roles/variables.tf
@@ -0,0 +1,14 @@
+variable "github_org" {
+  type        = string
+  description = "GitHub organization"
+}
+
+variable "github_repo" {
+  type        = string
+  description = "GitHub repository"
+}
+
+variable "environment" {
+  type        = string
+  description = "Deployment environment"
+}
