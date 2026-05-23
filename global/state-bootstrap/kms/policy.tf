187e0218c3aeda93d7992596af410ea6185195a9
wladysson <wladysonaraujo991@gmail.com>
2026-05-22 23:36:33 -0300

    feat: implementa policy document da chave KMS, prepara integração futura com IAM roles e serviços AWS

---
+++ global/state-bootstrap/kms/policy.tf
@@ -0,0 +1,25 @@
+data "aws_caller_identity" "current" {}
+
+data "aws_iam_policy_document" "kms" {
+
+  statement {
+    sid = "RootPermissions"
+
+    effect = "Allow"
+
+    principals {
+      type = "AWS"
+      identifiers = [
+        "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
+      ]
+    }
+
+    actions = [
+      "kms:*"
+    ]
+
+    resources = [
+      "*"
+    ]
+  }
+}
