3880a7e6c15722b48110f70bec8dedd12beb6ee2
wladysson <wladysonaraujo991@gmail.com>
2026-05-22 23:30:29 -0300

    feat: implementar locking do Terraform com DynamoDB, implementa LockID como chave primária

---
+++ global/state-bootstrap/dynamodb-locks/table.tf
@@ -0,0 +1,17 @@
+resource "aws_dynamodb_table" "tfstate_lock" {
+  name         = var.lock_table_name
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  tags = merge(
+    local.common_tags,
+    {
+      Name = var.lock_table_name
+    }
+  )
+}
