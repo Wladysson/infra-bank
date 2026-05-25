7d25b6f5e148c08994269ff0137ef28765cb69b9
wladysson <wladysonaraujo991@gmail.com>
2026-05-25 03:57:36 -0300

    feat: implementar política de segurança do bucket tfstate, reforça transporte seguro do backend remoto

---
+++ global/state-bootstrap/s3-tfstate/policy.tf
@@ -0,0 +1,33 @@
+data "aws_iam_policy_document" "tfstate" {
+
+  statement {
+    sid = "DenyInsecureTransport"
+
+    effect = "Deny"
+
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+
+    actions = [
+      "s3:*"
+    ]
+
+    resources = [
+      aws_s3_bucket.tfstate.arn,
+      "${aws_s3_bucket.tfstate.arn}/*"
+    ]
+
+    condition {
+      test     = "Bool"
+      variable = "aws:SecureTransport"
+      values   = ["false"]
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "tfstate" {
+  bucket = aws_s3_bucket.tfstate.id
+  policy = data.aws_iam_policy_document.tfstate.json
+}
