b15c165f4bd5d49a67ad9683bae4ad462c617eb1
wladysson <wladysonaraujo991@gmail.com>
2026-05-26 23:44:25 -0300

    feat: implementar instalação base do Istio Service Mesh, adiciona instalação do Istio Base via Helm, estabelece baseline inicial do service mesh da plataforma

---
+++ terraform/modules/platform/service-mesh/main.tf
@@ -0,0 +1,19 @@
+resource "helm_release" "istio_base" {
+  name       = "istio-base"
+  repository = "https://istio-release.storage.googleapis.com/charts"
+  chart      = "base"
+  namespace  = "istio-system"
+
+  create_namespace = true
+}
+
+resource "helm_release" "istiod" {
+  name       = "istiod"
+  repository = "https://istio-release.storage.googleapis.com/charts"
+  chart      = "istiod"
+  namespace  = "istio-system"
+
+  depends_on = [
+    helm_release.istio_base
+  ]
+}
